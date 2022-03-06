8330e0ed26d1bac6d82bbd70b73703cd5707603f
Hessam Imen <hessam.imen@gmail.com>
2022-03-06 21:17:09 +1100

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution here!
+let pets = ["Milo", "Otis", "Garfield"];
+
+let append = [...pets, 'Odie'];
+const prepend = ['Odie', ...pets];
+const removeLast = pets.slice(0,pets.length-1);
+const removeFirst = pets.slice(1);
